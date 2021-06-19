f5e349eb8b3e68db22a1185c0f3b0bc7de89a6db
s22051-pj-lukasz-kos <s22051@pjwstk.edu.pl>
2021-06-19 16:11:07 +0200

    Exercises to 1.8 done

--- kod.1.5.2.c
+++ kod.1.5.2.c
@@ -1,11 +1,8 @@
 #include <stdio.h> 
 
 main() {
-    long nc = 0;
-    while (getchar() != EOF)
-    {
-        ++nc;
-        printf("%ld\n", nc);
-    }
-    
+    double nc;
+    for (nc=0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
 }
